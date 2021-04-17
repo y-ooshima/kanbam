2d67a3f8ddd325842277f6da3972d1c66707d4d3
y-ooshima <gozira.1kawa@gmail.com>
2021-04-17 09:00:30 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/','ListingsController@index');
+
+Route::get('/new', 'ListingsController@new')->name('new');
+
+Route::post('/listings','ListingsController@store');
+
+Route::get('/listingsedit/{listing_id}', 'ListingsController@edit');
+
+Route::post('/listing/edit','ListingsController@update');
+
+Route::get('/listingsdelete/{listing_id}', 'ListingsController@destroy');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('listing/{listing_id}/card/new', 'CardsController@new')->name('new_card');
+
+Route::post('/listing/{listing_id}/card', 'CardsController@store');
+
+Route::get('listing/{listing_id}/card/{card_id}', 'CardsController@show');
+
+Route::get('listing/{listing_id}/card/{card_id}/edit', 'CardsController@edit');
+
+Route::post('/card/edit', 'CardsController@update');
+
+Route::get('listing/{listing_id}/card/{card_id}/delete', 'CardsController@destroy');
